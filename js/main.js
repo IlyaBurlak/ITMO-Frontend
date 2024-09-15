5bd4b129edc2d9761d71fa3086d803e38df8914d
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2024-09-15 13:19:33 +0300

    refactor: add Comments

--- js/main.js
+++ js/main.js
@@ -1,67 +1,148 @@
-const btnDarkMode = document.querySelector(".dark-mode-btn");
+document.addEventListener('DOMContentLoaded', () => {
+    const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// 1. Проверка темной темы на уровне системных настроек
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-	document.body.classList.add("dark");
-}
+    // Проверка темной темы в системных настройках
+    if (btnDarkMode) {
+        if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark");
+        }
 
-// 2. Проверка темной темы в localStorage
-if (localStorage.getItem('darkMode') === 'dark') {
-    btnDarkMode.classList.add("dark-mode-btn--active");
-    document.body.classList.add("dark");
-} else if (localStorage.getItem("darkMode") === "light") {
-    btnDarkMode.classList.remove("dark-mode-btn--active");
-    document.body.classList.remove("dark");
-}
+        if (localStorage.getItem('darkMode') === 'dark') {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark");
+        } else if (localStorage.getItem("darkMode") === "light") {
+            btnDarkMode.classList.remove("dark-mode-btn--active");
+            document.body.classList.remove("dark");
+        }
 
-// Если меняются системные настройки, меняем тему
-window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (event) => {
-        const newColorScheme = event.matches ? "dark" : "light";
+        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+            const newColorScheme = event.matches ? "dark" : "light";
+            if (newColorScheme === "dark") {
+                btnDarkMode.classList.add("dark-mode-btn--active");
+                document.body.classList.add("dark");
+                localStorage.setItem("darkMode", "dark");
+            } else {
+                btnDarkMode.classList.remove("dark-mode-btn--active");
+                document.body.classList.remove("dark");
+                localStorage.setItem("darkMode", "light");
+            }
+        });
 
-        if (newColorScheme === "dark") {
-			btnDarkMode.classList.add("dark-mode-btn--active");
-			document.body.classList.add("dark");
-			localStorage.setItem("darkMode", "dark");
-		} else {
-			btnDarkMode.classList.remove("dark-mode-btn--active");
-			document.body.classList.remove("dark");
-			localStorage.setItem("darkMode", "light");
-		}
+        btnDarkMode.onclick = function () {
+            btnDarkMode.classList.toggle("dark-mode-btn--active");
+            const isDark = document.body.classList.toggle("dark");
+            localStorage.setItem("darkMode", isDark ? "dark" : "light");
+        };
+    }
+
+    // Время загрузки страницы
+    window.addEventListener('load', () => {
+        let loadTime = performance.now();
+        const footer = document.getElementById('load-time');
+        if (footer) {
+            footer.innerHTML = 'Время загрузки страницы: ' + loadTime.toFixed(2) + ' миллисекунд';
+        }
     });
 
-// Включение ночного режима по кнопке
-btnDarkMode.onclick = function () {
-    btnDarkMode.classList.toggle("dark-mode-btn--active");
-    const isDark = document.body.classList.toggle("dark");
-
-    if (isDark) {
-        localStorage.setItem("darkMode", "dark");
-    } else {
-        localStorage.setItem("darkMode", "light");
+    // Загрузка CV
+    const downloadButtonCV = document.getElementById('CV-btn');
+    if (downloadButtonCV) {
+        downloadButtonCV.addEventListener('click', (event) => {
+            const link = document.createElement("a");
+            link.href = './info/CV.docx';
+            link.download = 'IlyaBurlakCV.docx';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
     }
-};
 
-// Загрузка CV
-const downloadButtonCV = document.getElementById('CV-btn')
+    const commentForm = document.getElementById('comment-form');
+    const submitButton = document.querySelector('button[type="submit"]');
 
-downloadButtonCV.addEventListener('click', (event) => {
-    const link = document.createElement("a");
-    link.href = './info/CV.docx';
-    link.download = 'IlyaBurlakCV.docx'
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    // Очистка комментариев
+    const clearCommentsButton = document.getElementById('clear-comments-btn');
+    if (clearCommentsButton) {
+        clearCommentsButton.addEventListener('click', () => {
+            localStorage.removeItem('comments'); // Очистить localStorage
+            const commentsContainer = document.querySelector('.comments-container');
+            commentsContainer.innerHTML = ''; // Очистить контейнер комментариев
+        });
+    }
+
+    // Проверка существующих комментариев и отображение их на странице
+    const storedComments = JSON.parse(localStorage.getItem('comments'));
+    if (storedComments) {
+        storedComments.forEach((comment, index) => {
+            const newComment = createCommentElement(comment.name, comment.surname, comment.telegram, comment.commentText, index);
+            const commentsContainer = document.querySelector('.comments-container');
+            commentsContainer.appendChild(newComment);
+        });
+    }
+
+    // Обработчик отправки формы для комментариев
+    if (commentForm && submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            const name = document.getElementById('nameC').value;
+            const surname = document.getElementById('surname').value;
+            const telegram = document.getElementById('telegram').value;
+            const commentText = document.getElementById('comment').value;
+
+            const newComment = createCommentElement(name, surname, telegram, commentText);
+
+            const comments = JSON.parse(localStorage.getItem('comments')) || [];
+            comments.push({ name, surname, telegram, commentText });
+            localStorage.setItem('comments', JSON.stringify(comments));
+
+            const commentsContainer = document.querySelector('.comments-container');
+            commentsContainer.appendChild(newComment);
+
+            commentForm.reset();
+        });
+    }
+
+    // Функция для создания элемента комментария
+    function createCommentElement(name, surname, telegram, commentText, index) {
+        const commentDiv = document.createElement('div');
+        commentDiv.classList.add('comment');
+        const nameHeader = document.createElement('h3');
+        nameHeader.textContent = `${name} ${surname}`;
+        commentDiv.appendChild(nameHeader);
+        commentDiv.dataset.index = index;
+
+        if (telegram) {
+            const telegramLink = document.createElement('a');
+            telegramLink.href = `https://t.me/${telegram}`;
+            telegramLink.textContent = `@${telegram}`;
+            telegramLink.target = '_blank';
+            const telegramParagraph = document.createElement('p');
+            telegramParagraph.textContent = 'Telegram: ';
+            telegramParagraph.appendChild(telegramLink);
+            commentDiv.appendChild(telegramParagraph);
+        }
+        const commentParagraph = document.createElement('p');
+        commentParagraph.textContent = commentText;
+        commentDiv.appendChild(commentParagraph);
+
+        // Кнопка удаления комментария
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('comment__delete-btn');
+        deleteButton.textContent = 'Удалить';
+        commentDiv.appendChild(deleteButton);
+
+        // Обработчик события для удаления
+        deleteButton.addEventListener('click', () => {
+            commentDiv.remove();
+            const storedComments = JSON.parse(localStorage.getItem('comments'));
+            if (storedComments) {
+                storedComments.splice(commentDiv.dataset.index, 1);
+                localStorage.setItem('comments', JSON.stringify(storedComments));
+            }
+        });
+
+        return commentDiv;
+    }
 });
 
-(function() {
-    window.addEventListener('load', function () {
-        let loadTime = performance.now();
-
-        let footer = document.getElementById('load-time');
-
-        footer.innerHTML = 'Время загрузки страницы: ' + loadTime.toFixed(2) + ' миллисекунд';
-    });
-})();
